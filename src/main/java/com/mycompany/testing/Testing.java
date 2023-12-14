a278722d22d8e51e54ad01816a25ae189ddeb532
isaacLopez2434 <ijlr30042004@gmail.com>
2023-12-13 19:07:34 -0500

    first commit

---
+++ src/main/java/com/mycompany/testing/Testing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testing;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Testing {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Worlds!");
+    }
+}
